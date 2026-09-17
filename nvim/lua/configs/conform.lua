f0dd55bfa2e8551a7a0d4b7f5282c3b044ec144c
L4J <johndoe@somthing.com>
2026-09-17 18:43:17 -0400

    nvim config

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,29 @@
+local options = {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+    },
+
+    formatters = {
+        ["clang-format"] = {
+            prepend_args = {
+                "-style={ \
+                IndentWidth: 4, \
+                TabWidth: 4, \
+                UseTab: Never, \
+                AccessModifierOffset: 0, \
+                IndentAccessModifiers: true, \
+                PackConstructorInitializers: Never}",
+            },
+        },
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+}
+
+require("conform").setup(options)
